78062b1e3e98e1e7354b54a1092fc52baba1aaee
Edward Lopez <edwardlopez2824@gmail.com>
2021-02-20 22:51:42 -0500

    [test] A cash register can get denominations to returned

--- app/Models/CashRegister.php
+++ app/Models/CashRegister.php
@@ -31,8 +31,64 @@ class CashRegister extends Model
     public static function getAllDenominations()
     {
         return self::all()->reduce(function($aco, $cash){
-            $aco[$cash->denomination] = intval($cash->quantity);
+            $aco[$cash->denomination] = \intval($cash->quantity);
             return $aco;
         }, []);
+    }
+
+    public static function getTotalDeposited($denominations)
+    {
+        $totalDeposited = 0;
+
+        foreach($denominations as $denomination => $quantity) {
+            $totalDeposited += self::getTheValueOfTheDenomination($denomination) * $quantity;
+        }
+
+        return $totalDeposited;
+    }
+
+    public static function getTheValueOfTheDenomination($denomination)
+    {
+        return \intval(str_replace('D', '', $denomination));
+    }
+
+    public static function getDenominationsToReturned($amountPaid, $totalDeposited)
+    {
+        $denominationsToReturned = [];
+
+        $change = $totalDeposited - $amountPaid;
+
+        $denominationsDB = self::getAllDenominations();
+
+        while($change > 0) {
+            $maxDenomination = self::getMaxPosibleDenominationChange($change, $denominationsDB);
+
+            $denomination = "D{$maxDenomination}";
+
+            $denominationsDB[$denomination] -= 1;
+
+            $change -= $maxDenomination;
+            
+            $denominationsToReturned[$denomination] = isset($denominationsToReturned[$denomination]) 
+                ? ($denominationsToReturned[$denomination] + 1) 
+                : 1;
+        }
+
+        return $denominationsToReturned;
+    }
+
+    public static function getMaxPosibleDenominationChange($change, $denominations) 
+    {
+        $denominations = \array_keys($denominations);
+
+        $denominations = \array_map(['self', 'getTheValueOfTheDenomination'], $denominations);
+
+        \sort($denominations);
+
+        $denominations = \array_filter($denominations, function($denomination) use ($change){
+            return ($denomination <= $change);
+        });
+
+        return \array_pop($denominations);
     }
 }
--- tests/Unit/Models/CashRegisterTest.php
+++ tests/Unit/Models/CashRegisterTest.php
@@ -46,4 +46,43 @@ class CashRegisterTest extends TestCase
 
         $this->assertEquals($denominations, $denominationsDB);
     }
+
+    /** @test */
+    public function a_cash_register_can_get_total_deposited()
+    {
+        $denominations = [
+            'D100000' => 1, 'D50000' => 1, 'D20000' => 1, 'D10000' => 1, 'D5000' => 1, 'D1000' => 1, 'D500' => 1, 'D200' => 1, 'D100' => 1, 'D50' => 1,
+        ];
+
+        $totalDeposited = CashRegister::getTotalDeposited($denominations);
+
+        $this->assertEquals(186850, $totalDeposited);
+    }
+
+    /** @test */
+    public function a_cash_register_can_get_the_value_of_the_denomination()
+    {
+        $this->assertEquals(
+            50000,
+            CashRegister::getTheValueOfTheDenomination('D50000')
+        );
+    }
+
+    /** @test */
+    public function a_cash_register_can_get_denominations_to_returned()
+    {
+        // Given
+        $denominations = [
+            'D100000' => 10, 'D50000' => 10, 'D20000' => 10, 'D10000' => 10, 'D5000' => 10, 'D1000' => 10, 'D500' => 10, 'D200' => 10, 'D100' => 10, 'D50' => 10,
+        ];
+
+        CashRegister::updateAllDenominations($denominations);
+
+        $denominationsToReturned = CashRegister::getDenominationsToReturned(10000, 50000);
+
+        $this->assertEquals(
+            ['D20000' => 2],
+            $denominationsToReturned
+        );
+    }
 }
